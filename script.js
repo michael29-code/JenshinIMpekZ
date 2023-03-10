e6256637bf9c36d3264ce493fc54d1e105c9e558
michael <alf.michael29@gmail.com>
2023-03-10 14:21:57 +0700

    home, navigation responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("navigation-mobile").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("navigation-mobile").style.width = "0";
+  document.body.style.overflow = "scroll";
+}
